fea8ee93eea247b10a6052f70af0a89a062ffe15
January Shen <januaryshen@JanuarytekiMacBook-Air.local>
2023-11-18 17:56:17 -0800

    Add path for three models

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import OtherExpensesCreate, UserCreate, MileageDataCreate
+from .views import OtherExpensesCreate, UserCreate, MileageDataCreate, OtherExpensesList, OtherExpensesUpdate
 
 urlpatterns = [
-    path('expenses/', OtherExpensesCreate.as_view(), name='expenses-create'),
-    path('user/', UserCreate.as_view(), name='user-create'),
-    path('mileageData/', MileageDataCreate.as_view(), name='mileageData-create'),
+    path('expenses/', OtherExpensesCreate.as_view(),
+         name='other-expenses-create'),
+    path('expenses/list/', OtherExpensesList.as_view(),
+         name='other-expenses-list'),
+    path('expenses/<int:pk>/', OtherExpensesUpdate.as_view(),
+         name='other-expenses-detail'),
+
+    path('mileage/', MileageDataAPIView.as_view(), name='mileage-list-create'),
+    path('mileage/<int:pk>/', MileageDataAPIView.as_view(),
+         name='mileage-update-delete'),
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,29 +1,56 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import OtherExpenses
+from .models import MileageData, OtherExpenses, User 
 from .serializers import OtherExpensesSerializer, UserSerializer, MileageDataSerializer
 
-class OtherExpensesCreate(APIView):
-    def post(self, request, format=None):
-        serializer = OtherExpensesSerializer(data=request.data)
+class GenericAPIView(generics.GenericAPIView):
+    serializer_class = None
+    queryset = None
+
+    # GET method for listing items or getting a single item
+    def get(self, request, *args, **kwargs):
+        if 'pk' in kwargs:
+            # Get a single item
+            item = generics.get_object_or_404(self.get_queryset(), pk=kwargs['pk'])
+            serializer = self.get_serializer(item)
+        else:
+            # List items
+            queryset = self.filter_queryset(self.get_queryset())
+            serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    # POST method for creating an item
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class UserCreate(APIView):
-    def post(self, request, format=None):
-        serializer = UserSerializer(data=request.data)
+    # PATCH method for updating an item
+    def patch(self, request, *args, **kwargs):
+        item = generics.get_object_or_404(self.get_queryset(), pk=kwargs['pk'])
+        serializer = self.get_serializer(item, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-class MileageDataCreate(APIView):
-    def post(self, request, format=None):
-        serializer = MileageDataSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # DELETE method for deleting an item
+    def delete(self, request, *args, **kwargs):
+        item = generics.get_object_or_404(self.get_queryset(), pk=kwargs['pk'])
+        item.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class MileageDataAPIView(GenericAPIView):
+    queryset = MileageData.objects.all()
+    serializer_class = MileageDataSerializer
+
+class OtherExpensesAPIView(GenericAPIView):
+    queryset = OtherExpenses.objects.all()
+    serializer_class = OtherExpensesSerializer
+
+class UserAPIView(GenericAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer      
