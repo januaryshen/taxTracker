53e3f0dfc36ea3731fc4157232ad6f73cd91f0ff
January Shen <januaryshen@JanuarytekiMacBook-Air.local>
2023-11-18 19:56:20 -0800

    Constructed three classes for api endpoint

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,15 +1,11 @@
 from django.urls import path
-from .views import OtherExpensesCreate, UserCreate, MileageDataCreate, OtherExpensesList, OtherExpensesUpdate
+from .views import MileageDataAPIView, OtherExpensesAPIView, UserAPIView
 
 urlpatterns = [
-    path('expenses/', OtherExpensesCreate.as_view(),
-         name='other-expenses-create'),
-    path('expenses/list/', OtherExpensesList.as_view(),
-         name='other-expenses-list'),
-    path('expenses/<int:pk>/', OtherExpensesUpdate.as_view(),
-         name='other-expenses-detail'),
-
     path('mileage/', MileageDataAPIView.as_view(), name='mileage-list-create'),
-    path('mileage/<int:pk>/', MileageDataAPIView.as_view(),
-         name='mileage-update-delete'),
+    path('mileage/<int:pk>/', MileageDataAPIView.as_view(),name='mileage-update-delete'),
+    path('expenses/', OtherExpensesAPIView.as_view(), name='expenses-list-create'),
+    path('expenses/<int:pk>/', OtherExpensesAPIView.as_view(),name='expenses-update-delete'),
+    path('user/', UserAPIView.as_view(), name='user-list-create'),
+    path('user/<int:pk>/', UserAPIView.as_view(),name='user-update-delete'),
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics
 from .models import MileageData, OtherExpenses, User 
 from .serializers import OtherExpensesSerializer, UserSerializer, MileageDataSerializer
 
